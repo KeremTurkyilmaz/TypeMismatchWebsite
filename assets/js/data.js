ac3a76f08179324cf680fe82e6bee975239bf8b7
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2019-01-14 16:28:26 +0100

    First Layout

---
+++ assets/js/data.js
@@ -0,0 +1,196 @@
+const data = [
+    {
+        "data": "10.01.2019", 
+        "desc": "Nella computer grafica \u00e8 un oggetto che viene descritto attraverso espressioni matematiche pure, dunque non esistono dei vertici che ne delimitano la forma. Quando un oggetto meta incontra un altro oggetto subisce la sua influenza creando cosi un effetto di fusione.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Metaballs/image/Metaballs.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Metaballs/image/Metaballs.png", 
+        "tags": [
+            "processing", 
+            "particelle"
+        ], 
+        "titolo": "Metaballs", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Metaballs"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Macchina da scrivere digitale, quando si preme ENTER le lettere diventano delle particelle che si muovono nello spazio lasciando dietro di loro una scia.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Lettere%20volanti/image/LettereVolanti.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Lettere%20volanti/image/LettereVolanti.png", 
+        "tags": [
+            "text", 
+            "particle", 
+            "processing"
+        ], 
+        "titolo": "Lettere volanti", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Lettere volanti"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Script che genera diversi elementi tipografici per vedere come si comporta il font. \u00c8 possibile scegliere il nome del font o generarlo random.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Scheda%20Tecnica%20Font/image/SchedaTecnicaFont.jpg", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Scheda%20Tecnica%20Font/image/SchedaTecnicaFont.jpg", 
+        "tags": [
+            "basiljs", 
+            "automazione", 
+            "tipografia", 
+            "font"
+        ], 
+        "titolo": "Scheda tecnica Font", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Scheda Tecnica Font"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Vengono caricate una serie di immagini che presentano della tipografia. Attraverso il movimento del mouse (X,Y) \u00e8 possibile distorcere l'immagine e ottenere composizione interessanti. Il modo 1 agisce su X,Y mentre il modo 2 solo sulla X e il modo 3 solo sulla Y.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Tipografia%20Distorta/image/TipografiaDistorta00.jpg", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Tipografia%20Distorta/image/TipografiaDistorta00.jpg", 
+        "tags": [
+            "processing", 
+            "tipografia", 
+            "distorsione", 
+            "immagini"
+        ], 
+        "titolo": "Tipografia distorta", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Tipografia Distorta"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Attraverso la rotazione e la dimensione di un immagine si possono creare delle forme complesse che si modificano in modo costante. Lo sfondo non viene resettato per permettere la creazione della scia, che altrimenti risulterebbe invisibile.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Psichedelico/image/Psichedelico00.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Psichedelico/image/Psichedelico00.png", 
+        "tags": [
+            "processing", 
+            "color", 
+            "image"
+        ], 
+        "titolo": "Psichedelico", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Psichedelico"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Come tutelarsi dai selfie. Il programma riconosce in maniera automatica il volto umano e applica di conseguenza una serie di forme per rendere irriconoscibile la persona.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Anti%20Selfie/image/AntiSelfie.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Anti%20Selfie/image/AntiSelfie.png", 
+        "tags": [
+            "processing", 
+            "video"
+        ], 
+        "titolo": "Anti Selfie", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Anti Selfie"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Una serie di comandi Bash che permettono di semplificare il flusso di lavoro. Bisogna fare attenzione per\u00f2 alle librerie richieste (mogrify, ffmpeg).", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Cubo%20RGB/image/CuboRGB02.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Cubo%20RGB/image/CuboRGB02.png", 
+        "tags": [
+            "bash"
+        ], 
+        "titolo": "Comandi Bash", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Comandi Bash"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "La parola Mandala viene dal sanscrito e significa cerchio, ciclo. Ci si riferisce dunque ad un disegno complesso che si compone rispetto ad un centro. Maggiore \u00e8 il numero di livelli e pi\u00f9 complesso sar\u00e0 il disegno finale.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Mandala/image/Mandala02.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Mandala/image/Mandala02.png", 
+        "tags": [
+            "processing", 
+            "grid", 
+            "draw"
+        ], 
+        "titolo": "Mandala", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Mandala"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Macchina da scrivere digitale, quando si preme ENTER le lettere diventano delle particelle che si muovono nello spazio lasciando dietro di loro una scia.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Lettere%20volanti/image/LettereVolanti.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Lettere%20volanti/image/LettereVolanti.png", 
+        "tags": [
+            "text", 
+            "particle", 
+            "processing"
+        ], 
+        "titolo": "Lettere volanti", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Libro in 3D"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Sovrapposizione di celle, viene preso un numero random da 1 a 3 che determina il colore delle traccia (R, G, B). Ogni tot secondi viene cancellato lo sfondo per far emergere un altro pattern.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Celle%20RGB/image/CelleRGB.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Celle%20RGB/image/CelleRGB.png", 
+        "tags": [
+            "griglia", 
+            "processing", 
+            "rgb", 
+            "moire"
+        ], 
+        "titolo": "Celle RGB", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Celle RGB"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Figura di interferenza che crea un movimento ottico grazie alla sovrapposizione di due griglie simili.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Moir\u00e9/image/Moire.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Moir\u00e9/image/Moire.png", 
+        "tags": [
+            "processing", 
+            "moire"
+        ], 
+        "titolo": "Moir\u00e9", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Moir\u00e9"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Semplice visualizzazione di tre cubi (R, G, B) in uno spazio 3D. \u00c8 stata inoltre introdotta una camera per cambiare manualmente la prospettiva di visualizzazione.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Cubo%20RGB/image/CuboRGB02.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Cubo%20RGB/image/CuboRGB02.png", 
+        "tags": [
+            "colore", 
+            "3d", 
+            "processing"
+        ], 
+        "titolo": "Cubo RGB", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Cubo RGB"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Permette di comparare in maniera veloce una serie di font. \u00c8 possibile scegliere se ottenere anche il nome o vedere solamente le lettere. Maggiore \u00e8 il numero di celle pi\u00f9 interessante diventa la tavola.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Tavola%20Font/image/TavolaFont.jpg", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Tavola%20Font/image/TavolaFont.jpg", 
+        "tags": [
+            "basiljs", 
+            "automazione", 
+            "tipografia", 
+            "font"
+        ], 
+        "titolo": "Tavola font", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Tavola Font"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Attraverso un piccolo script \u00e8 possibile creare un intero libro in modo automatico che mostra tutti i font che l'utente ha installato sul proprio computer. L'output \u00e8 un PDF in alta qualit\u00e0.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Tutti%20i%20font%20sul%20mio%20disco%20rigido/image/TuttiIFontSulMioDiscoRigido.jpg", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Tutti%20i%20font%20sul%20mio%20disco%20rigido/image/TuttiIFontSulMioDiscoRigido.jpg", 
+        "tags": [
+            "basiljs", 
+            "automazione", 
+            "tipografia", 
+            "font"
+        ], 
+        "titolo": "Tutti i font sul mio disco rigido", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Tutti i font sul mio disco rigido"
+    }, 
+    {
+        "data": "10.01.2019", 
+        "desc": "Viene caricato un file audio che modifica e colora, in base alla suono, i diversi rettangoli presenti nello spazio 3D.", 
+        "img_url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/blob/master/Visualizzazione%20musicale/image/VisualizzazioneAudio.png", 
+        "img_url_abs": "https://raw.githubusercontent.com/KeremTurkyilmaz/TypeMistmatchSketch/master/Visualizzazione%20musicale/image/VisualizzazioneAudio.png", 
+        "tags": [
+            "processing", 
+            "audio"
+        ], 
+        "titolo": "Visualizzazione audio", 
+        "url": "https://github.com/KeremTurkyilmaz/TypeMistmatchSketch/tree/master/Visualizzazione musicale"
+    }
+]
---
+++ assets/js/main.js
@@ -0,0 +1,102 @@
+window.addEventListener("load", function(e){
+
+	// extract all tags:
+	let tags = data.map(function(obj, item){
+		return obj.tags;
+	});
+
+	// flatten the array:
+	tags = flatten(tags);
+	
+	// get unique elements:
+	tags = tags.filter(function(value, index, self){
+		return self.indexOf(value) === index;
+	});
+
+	// sort array:
+	tags.sort();
+
+	// render tags:
+	const el_tags = document.querySelector(".tags");
+
+
+	tags.forEach(function(el){		
+		const li = document.createElement("li");
+		li.innerHTML = el;
+		li.classList.add("button-tag");
+		li.dataset.tag = el;
+		li.dataset.active = false;
+		el_tags.appendChild(li);
+		li.addEventListener('click', function(event){
+			const t = event.target;	
+			const btns = document.querySelectorAll(".button-tag");
+			const projects = document.querySelectorAll(".sketch");
+			if (t.dataset.active == 'false') {	
+				// filtro attivato	
+				for (btn of btns) {
+					btn.dataset.active = false;
+					btn.classList.add("disabled");
+				}		
+				t.dataset.active = true;			
+				t.classList.remove("disabled");
+			
+				for (prj of projects) {
+					if (prj.classList.contains("tag-" + t.dataset.tag)) {
+						prj.classList.remove("disabled");
+					} else {
+						prj.classList.add("disabled");
+					}
+				}
+			} else {
+				for (btn of btns) {
+					btn.classList.remove("disabled");
+					btn.dataset.active = false;
+				}	
+				for (prj of projects) {
+					prj.classList.remove("disabled");
+				}					
+			}
+		});
+	});
+
+	// render sketches:
+	const el_sketches = document.querySelector(".sketches");
+
+	data.forEach(function(el){		
+		const holder = document.createElement("div");
+		holder.classList.add("sketch");
+		holder.style.backgroundImage = 'url(' + el.img_url_abs + ')';
+		el.tags.forEach(function(tag){
+			holder.classList.add("tag-" + tag);
+		})
+		let html = "";
+		html += "<div class='descrizione'>";
+		html += "<p>"+el.titolo+"</p>";
+		html += "<p class='reveal'>"+el.data+"</p>";
+		html += "<p class='reveal'>"+el.desc+"</p>";
+		html += "<p class='reveal'>";
+		for (t of el.tags) {
+			html += "#" + t + " ";
+ 		}
+		html +="</p>";
+		html += "<p class='reveal'><a target='_blank' href='"+el.url+"'>GitHub</a></p>";
+		html += "</div>";
+
+		holder.innerHTML = html;
+		el_sketches.appendChild(holder);
+	});
+
+	// flattens an array:
+	function flatten(arr, result = []) {
+		for (let i = 0, length = arr.length; i < length; i++) {
+			const value = arr[i];
+				if (Array.isArray(value)) {
+		  			flatten(value, result);
+				} else {
+		  			result.push(value);
+				}
+			}
+		return result;
+	};
+})
+
